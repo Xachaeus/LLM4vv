424398a2043b4846091649d188274afa1e6f5983
Xachaeus <zack.sollenberger@gmail.com>
2024-05-30 11:32:53 -0400

    Updated GitHubRepoScraper.py

--- GitHubRepoScraper.py
+++ GitHubRepoScraper.py
@@ -1,16 +1,33 @@
 from github import Auth, Github
-import time
+import time, json
 
-repos_to_scrape = ["UD-CRPL/ppm_one", "matt-stack/PhysiCell_GPU", "uhhpctools/openacc-npb",
-                   "NCAR/MURaM_main"]
-extensions_to_check = [".cpp",".c"]
-headers_to_check = [".h"]
-libraries_to_check = ["omp.h", "openacc.h"]
+repos_to_scrape = ["OpenACCUserGroup/OpenACCV-V", "UD-CRPL/ppm_one", "matt-stack/PhysiCell_GPU", "uhhpctools/openacc-npb",
+                   "NCAR/MURaM_main", ]
+extensions_to_check = [".cpp",".c", ".F90"]
+headers_to_check = [".h", ".Fh"]
+c_libraries_to_check = ["omp.h", "openacc.h", "#pragma omp", "#pragma acc"]
+f_libraries_to_check = ["USE OPENACC", "USE OPENMP"]
 destination_file = "Test_Files/Passing_C-CPP_Codes.txt"
 file_separator = "\n########## NEXT FILE ##########\n"
+do_logging = True
 
 
-def search_for_text_alt(files, text):
+
+class Clock:
+    
+    def __init__(self):
+        self.start_time = time.time()
+    
+    def reset(self):
+        self.start_time = time.time()
+    def log(self):
+        log(f"Time Elapsed: {time.time()-self.start_time}s")
+
+def log(*args, **kwargs):
+    global do_logging
+    if do_logging: print(*args, **kwargs)
+    
+def search_for_text(files, text):
     passing_files = []
     for file in files:
         try:
@@ -20,49 +37,58 @@ def search_for_text_alt(files, text):
         
         for val in text:
             if content.find(val) > 0:
-                print(f"Found {val} in file {file.name}!")
+                log(f"Found {val} in file {file.name}!")
                 passing_files.append(file)
                 break
     return passing_files
 
-def search_for_text(files, text):
+
+
+def advanced_search(files, libraries, headers):
     passing_files = []
-    min_length = min([len(s) for s in text])
     for file in files:
-        passes = False #break out of loop if file passes
+        passes = False
+        log(f"\nChecking file {file.repository.name}/{file.name}... ", end="")
+        #passes = False #break out of loop if file passes
         try:
             content = file.decoded_content.decode()
         except:
+            log("Failure to read file!")
             continue
+        
+        for header in libraries:
+            if content.find(header)>0:
+                log(f"Found {header}!", end="")
+                passing_files.append(file)
+                passes = True
+                break
             
-        length = len(content)
-        for i in range(length//min_length): #iterate every x characters
-            if passes: break
-            c = content[i*min_length]
-            for val in text:
-                index = val.find(c)
-                if index<0: break #wrong character
-                
-                start = (i*min_length)-index
-                if content[start:start+len(val)] == val or content[start-1:start+len(val)-1] == val:
-                    print(f"Found {val} in file {file.name}!")
-                    passing_files.append(file)
-                    passes = True
-                    break
-                
+        if passes: continue
+        
+        for local_header in headers:
+            if local_header.repository.name != file.repository.name: continue
+            header = local_header.name
+            if content.find(header)>0:
+                log(f"Found {header}!", end="")
+                passing_files.append(file)
+                break
     return passing_files
 
 ###START OF SCRIPT
 
+clock = Clock()
+
 auth = Auth.Token("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")
 g = Github(auth=auth)
 
-target_files = []
-header_files = []
-
-#requires 
+c_target_files = []
+f_target_files = []
+c_header_files = []
+f_header_files = []
+#requires
+clock.reset()
 for name in repos_to_scrape:
-    print(f"Checking repo {name}...")
+    log(f"Getting content from repo {name}...")
     repo = g.get_repo(name)
     contents = repo.get_contents("")
     files = []
@@ -81,102 +107,47 @@ for name in repos_to_scrape:
         for extension in extensions_to_check:
             try:
                 if file.name[-(len(extension)):] == extension :
-                    target_files.append(file)
+                    c_target_files.append(file) if extension!=".F90" else f_target_files.append(file)
                     break
             except: pass
         for header in headers_to_check:
             try:
                 if file.name[-(len(header)):] ==  header:
-                    header_files.append(file)
+                    c_header_files.append(file) if header!=".Fh" else f_target_files.append(file)
                     break
             except: pass
+            
 
-print("Done checking repos!")
+log("Done checking repos!")
+clock.log()
+clock.reset()
 
 #Update header references
-print("\nUpdating header references...")
+log("\nUpdating header references...")
 
-passing_headers = search_for_text_alt(header_files, libraries_to_check)
+c_passing_headers = search_for_text(c_header_files, c_libraries_to_check)
+f_passing_headers = search_for_text(f_header_files, f_libraries_to_check)
 
-print("Done!")
+log("Done!")
+clock.log()
             
 #Find all files with specified text/desired libraries included
-passing_files = passing_headers
-min_length = min([len(s) for s in libraries_to_check])
+passing_files = c_passing_headers + f_passing_headers
+#min_length = min([len(s) for s in c_libraries_to_check])
+clock.reset()
+passing_files += advanced_search(c_target_files, c_libraries_to_check, c_passing_headers)
+passing_files += advanced_search(f_target_files, f_libraries_to_check, f_passing_headers)
 
-for file in target_files:
-    passes = False
-    print(f"Checking file {file.repository.name}/{file.name}... ", end="")
-    #passes = False #break out of loop if file passes
-    try:
-        content = file.decoded_content.decode()
-    except:
-        print("Failure to read file!")
-        continue
-    
-    for header in libraries_to_check:
-        if content.find(header)>0:
-            print(f"Found {header}!", end="")
-            passing_files.append(file)
-            passes = True
-            break
-    if passes: continue
-    for local_header in passing_headers:
-        if local_header.repository != file.repository: continue
-        header = local_header.name
-        if content.find(header)>0:
-            print(f"Found {header}!", end="")
-            passing_files.append(file)
-            break
-    
-    print("")
-    """  
-    length = len(content)
-    for i in range(length//min_length): #iterate every x characters
-        if passes: break
-        c = content[i*min_length]
-        for header in libraries_to_check:
-            index = header.find(c)
-            if index<0: break #wrong character
-            
-            start = (i*min_length)-index
-            if content[start:start+len(header)] == header or content[start-1:start+len(header)-1] == header:
-                print(f"Found {header}!", end="")
-                passing_files.append(file)
-                passes = True
-                break
-            
-        for local_header in passing_headers:
-            if local_header.repository != file.repository: continue
-            header = local_header.name #Reassigning name because I'm lazy
-            
-            #Figure out if c is in header and if header is in file
-            index = header.find(c)
-            if index<0: break #wrong character
-            header_found = False
-            
-            for j in range(len(header)):
-                if header[j] == c:
-                    start = (i*min_length) - j
-                    header_found = content[start:start+len(header)] == header
-                if header_found: break
-                    
-            if header_found:
-                print(f"Found {header}!", end="")
-                passing_files.append(file)
-                passes = True
-                break
-    """
 
-print("\nDone checking files! Passing files are:\n" + '\n'.join([f"{f.repository.name}/{f.name}" for f in passing_files]))
-            
-#Write passing files to single text file   
-full_string = ""
+
+log("\n\nDone checking files! Passing files are:\n" + '\n'.join([f"{f.repository.name}/{f.name}" for f in passing_files]))
+clock.log()
+
+final_string = ""
 for file in passing_files:
-    full_string += file.decoded_content.decode()
-    full_string += file_separator
+    final_string += file_separator + "::: " + file.name + " :::\n" + file.decoded_content.decode()
 with open(destination_file, 'w') as f:
-    f.write(full_string)
+    f.write(final_string)
             
     
 
